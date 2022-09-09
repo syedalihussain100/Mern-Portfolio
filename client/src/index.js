a10683444de3066baba1d0145a54e77bf607d650
syedalihussain100 <smuheminali@gmail.com>
2022-09-09 11:47:08 -0700

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import Store from "./Store";
 import { Provider as AlertProvider, positions, transitions } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-import { hydrate, render } from "react-dom";
 
 const options = {
   positions: positions.BOTTOM_CENTER,
@@ -14,7 +13,8 @@ const options = {
   transitions: transitions.SCALE,
 };
 
-const Apps = (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={Store}>
       <AlertProvider template={AlertTemplate} {...options}>
@@ -23,21 +23,3 @@ const Apps = (
     </Provider>
   </React.StrictMode>
 );
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(Apps, rootElement);
-} else {
-  render(Apps, rootElement);
-}
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={Store}>
-//       <AlertProvider template={AlertTemplate} {...options}>
-//         <App />
-//       </AlertProvider>
-//     </Provider>
-//   </React.StrictMode>
-// );
